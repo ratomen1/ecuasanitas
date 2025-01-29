fcf17e29f16555dbf4960c73c834e73a336564f1
Jose Gomez <geomateolol@gmail.com>
2025-01-29 09:32:12 -0500

    Query para carga masiva de empleados Ecuasanitas

---
+++ PRODUCCION_GENESYS/MiguelEmpleadosCargaMasiva.sql
@@ -0,0 +1,66 @@
+SELECT
+    c.nivel_id,
+    ti.nombre as tipo_identificacion,
+    e.numero as cedula,
+    e.primernombre,
+    e.segundonombre,
+    e.primerapellido,
+    e.segundoapellido,
+    e.email,
+    EXTRACT(YEAR FROM e.fechanacimiento) as anio_nacimiento,
+    EXTRACT(MONTH FROM e.fechanacimiento) as mes_nacimiento,
+    EXTRACT(DAY FROM e.fechanacimiento) as dia_nacimiento,
+    ep.nombre as parentesco,
+    case when e.discapacitado = true then 'SI' else 'NO' end as discapacitado,
+    CASE
+        WHEN e.estadocivil = 'SO' THEN 'SOLTERO'
+        WHEN e.estadocivil = 'CA' THEN 'CASADO'
+        WHEN e.estadocivil = 'DI' THEN 'DIVORCIADO'
+        WHEN e.estadocivil = 'VI' THEN 'VIUDO'
+        WHEN e.estadocivil = 'UN' THEN 'EN UNION DE HECHO'
+        WHEN e.estadocivil = 'SE' THEN 'SEPARADO'
+        WHEN e.estadocivil = 'TO' THEN 'TODOS'
+        ELSE 'DESCONOCIDO'
+        END as estado_civil,
+    case when e.genero = 'M' then 'MASCULINO' else 'FEMENINO' end as genero,
+    'SI' as transferencia,
+    c.numero as contratoOrigen,
+    '' as prepagaanterior,
+    coberturas.codigoservicio as coberturasindividuales,
+    '' as coberturasfamiliares,
+    '' as contratantefamilia,
+    '' as parentescocontratante
+FROM contrato c
+left join afiliacion a on c.id = a.contrato_id and a.estadoafiliacion = 'ACT'
+left join entrada ep on ep.id = a.parentesco_id
+left join entidad e on a.afiliado_id = e.id
+left join tipoidentificacion ti on e.tipoidentificacion_id = ti.id
+left join LATERAL (
+    SELECT
+        cc.afiliacion_id,
+        STRING_AGG(cc.codigoservicio::text, ',') AS codigoservicio
+    FROM coberturacontratada cc
+    WHERE cc.afiliacion_id = a.id
+      AND cc.estadocoberturacontratada = 'ACT'
+      AND cc.coberturaimplicita = FALSE
+    GROUP BY cc.afiliacion_id
+    ) as coberturas on coberturas.afiliacion_id = a.id
+WHERE c.numero in (564096,603294)
+and c.estado in ('ACT','SUS');
+
+
+SELECT
+    cc.afiliacion_id,
+    STRING_AGG(cc.codigoservicio::text, ',') AS codigoservicio
+FROM coberturacontratada cc
+WHERE cc.afiliacion_id = 190007310142036
+  AND cc.estadocoberturacontratada = 'ACT'
+  AND cc.coberturaimplicita = FALSE
+GROUP BY cc.afiliacion_id
+
+
+
+
+
+
+
