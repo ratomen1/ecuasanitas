1644903706ce5715b2558e6d566ec75234dfdf7f
Jose Gomez <geomateolol@gmail.com>
2025-03-11 08:17:00 -0500

    Add new SQL queries for documentacionasesor, preventaweb, contrato, and afiliacion

---
+++ PRODUCCION_GENESYS/2025-03-10.sql
@@ -0,0 +1,23 @@
+select * from documentacionasesor where id =  108396 --- 604410  SANTO DOMINGO
+
+--CASO DAVID
+select * from documentacionasesor where id =  108395  --  557029 SEDE NULL
+
+
+select * from preventaweb where preventaweb.numerocontrato is not null order by 1 desc
+
+select * from contrato where numero = 607992  --  galoedumd@hotmail.com
+
+select * FROM titular where contrato_id = 2008740
+
+select * from entidad where id = 41441  --  galoedumd@hotmail.com
+
+
+
+select * from entidad where numero = '1726679440'
+
+select * from afiliacion where afiliado_id =28255
+
+select * from afiliacion where familia_id =190007481243
+
+select * from coberturacontratada where familia_id = 190007481243
